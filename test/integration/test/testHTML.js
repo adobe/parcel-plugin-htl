c5380070e964788713ddb4a7b930b62ba35dec3d
Lars Trieloff <trieloff@adobe.com>
2018-06-27 11:46:09 +0000

    Test script execution

--- test/integration/test/testHTML.js
+++ test/integration/test/testHTML.js
@@ -1,9 +1,28 @@
 const assert = require("assert");
 
 describe("Testing html.htl Pipeline", () => {
+    const params = {};
+
     const script = require("../dist/html.js");
-    
+
     it("Script can be required", () => {
-      assert.ok(script);
+        assert.ok(script);
     });
+
+    it("Script has main function", () => {
+        assert.ok(script.main);
+        assert.equal(typeof script.main, "function");
+    });
+
+    it("Script has can be executed", (done) => {
+        const result = script.main(params);
+        assert.ok(result);
+        result.then((res) => {
+            assert.ok(res);
+            console.log(res);
+            done();
+        });
+    });
+
+    
 });
