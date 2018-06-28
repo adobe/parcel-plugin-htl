66e4b8768ab321e1dbc1e7657419ef2ebd35ed04
Lars Trieloff <trieloff@adobe.com>
2018-06-28 12:30:22 +0000

    Fixed lint

--- HTLAsset.js
+++ HTLAsset.js
@@ -9,48 +9,47 @@
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
-const Compiler = require("@adobe/htlengine/src/compiler/Compiler");
+const Compiler = require('@adobe/htlengine/src/compiler/Compiler');
 
-const JSAsset = require("parcel-bundler/src/assets/JSAsset");
-const fs = require("fs");
-const path = require("path");
+const JSAsset = require('parcel-bundler/src/assets/JSAsset');
+const fs = require('fs');
 
-const DEFAULT_PIPELINE = "@adobe/hypermedia-pipeline/src/defaults/default.js";
+const DEFAULT_PIPELINE = '@adobe/hypermedia-pipeline/src/defaults/default.js';
 
 class HTLAsset extends JSAsset {
   constructor(name, options) {
     super(name, options);
-    this.type = "js";
+    this.type = 'js';
   }
 
   async parse(code) {
     // TODO
     const compiler = new Compiler()
-      .withOutputDirectory("")
+      .withOutputDirectory('')
       .includeRuntime(true)
-      .withRuntimeGlobalName("it");
+      .withRuntimeGlobalName('it');
 
     this.contents = compiler.compileToString(code);
 
-    const rootname = this.name.replace(/\.[^.]+$/, "");
+    const rootname = this.name.replace(/\.[^.]+$/, '');
     const extension = this.basename
-      .split(".")
+      .split('.')
       .slice(-2, -1)
       .pop();
     const selector = this.basename
-      .split(".")
+      .split('.')
       .slice(0, -2)
-      .join(".");
+      .join('.');
 
     const pipe = HTLAsset.getPreprocessor(
       `${rootname}.pipe.js`,
       `@adobe/hypermedia-pipeline/src/defaults/${extension}.pipe.js`,
-      selector
+      selector,
     );
     const pre = HTLAsset.getPreprocessor(
       `${rootname}.pre.js`,
       `@adobe/hypermedia-pipeline/src/defaults/${extension}.pre.js`,
-      selector
+      selector,
     );
 
     // return super.parse(this.contents);
@@ -72,7 +71,7 @@ class HTLAsset extends JSAsset {
 
   static getPreprocessor(name, fallback) {
     if (fs.existsSync(name)) {
-      const relname = name.replace(/^.*\//g, "./");
+      const relname = name.replace(/^.*\//g, './');
       return relname;
     }
     try {
