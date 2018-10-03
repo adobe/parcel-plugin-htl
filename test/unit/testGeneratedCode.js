f5c640ce14669255095499023ec0bb533656f745
Tobias Bocanegra <tripod@adobe.com>
2018-10-03 11:19:30 +0900

    fix linting

--- test/unit/testGeneratedCode.js
+++ test/unit/testGeneratedCode.js
@@ -39,7 +39,7 @@ const TEST_SCRIPTS = [
 
 describe('Generated Code Tests', function suite() {
   this.timeout(10000);
-  
+
   TEST_SCRIPTS.forEach((testScript) => {
     const DIST_HTML_JS = path.resolve(__dirname, `../example/dist/${testScript}.js`);
     const DIST_HTML_CSS = path.resolve(__dirname, '../example/dist/bla.d80072a5.css');
